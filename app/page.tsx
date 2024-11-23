b017f2dcbc77b4ba0d5cd3f9a904755a35936ee2
Adnan <adnanali23189@gmail.com>
2024-11-23 13:36:16 +0530

    feat: bento grid of problem showcase

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
-import ProblemShowcaseDetail from "@/components/ProblemShowcaseDetail";
+import { ProblemShowcaseDetail } from "@/components/ProblemShowcaseDetail";
 import WayToSolve from "@/components/WayToSolve";
 
 export default function Home() {
--- components/ProblemShowcaseDetail.tsx
+++ components/ProblemShowcaseDetail.tsx
@@ -1,11 +1,153 @@
-import React from 'react'
+"use client";
+import { cn } from "@/lib/utils";
+import React from "react";
+import { BentoGrid, BentoGridItem } from "./ui/bento-grid";
+import {
+  IconBoxAlignRightFilled,
+  IconClipboardCopy,
+  IconFileBroken,
+  IconSignature,
+  IconTableColumn,
+} from "@tabler/icons-react";
+import { motion } from "framer-motion";
 
-function ProblemShowcaseDetail() {
+export function ProblemShowcaseDetail() {
   return (
-    <div>
-    ProblemShowcaseDetail
-    </div>
-  )
+    <BentoGrid className="max-w-4xl mx-auto md:auto-rows-[20rem]">
+      {items.map((item, i) => (
+        <BentoGridItem
+          key={i}
+          title={item.title}
+          description={item.description}
+          header={item.header}
+          className={cn("[&>p:text-lg]", item.className)}
+          icon={item.icon}
+        />
+      ))}
+    </BentoGrid>
+  );
 }
 
-export default ProblemShowcaseDetail
+const SkeletonOne = () => {
+  const variants = {
+    initial: {
+      x: 0,
+    },
+    animate: {
+      x: 10,
+      rotate: 5,
+      transition: {
+        duration: 0.2,
+      },
+    },
+  };
+  const variantsSecond = {
+    initial: {
+      x: 0,
+    },
+    animate: {
+      x: -10,
+      rotate: -5,
+      transition: {
+        duration: 0.2,
+      },
+    },
+  };
+
+  return (
+    <motion.div
+      initial="initial"
+      whileHover="animate"
+      className="flex flex-1 w-full h-full min-h-[6rem] dark:bg-dot-white/[0.2] bg-dot-black/[0.2] flex-col space-y-2"
+    >
+      <motion.div
+        variants={variants}
+        className="flex flex-row rounded-full border border-neutral-100 dark:border-white/[0.2] p-2 items-center space-x-2 bg-white dark:bg-black"
+      >
+        <div className="h-6 w-6 rounded-full bg-gradient-to-r from-pink-500 to-violet-500 flex-shrink-0" />
+        <div className="w-full bg-gray-100 h-4 rounded-full dark:bg-neutral-900" />
+      </motion.div>
+      <motion.div
+        variants={variantsSecond}
+        className="flex flex-row rounded-full border border-neutral-100 dark:border-white/[0.2] p-2 items-center space-x-2 w-3/4 ml-auto bg-white dark:bg-black"
+      >
+        <div className="w-full bg-gray-100 h-4 rounded-full dark:bg-neutral-900" />
+        <div className="h-6 w-6 rounded-full bg-gradient-to-r from-pink-500 to-violet-500 flex-shrink-0" />
+      </motion.div>
+      <motion.div
+        variants={variants}
+        className="flex flex-row rounded-full border border-neutral-100 dark:border-white/[0.2] p-2 items-center space-x-2 bg-white dark:bg-black"
+      >
+        <div className="h-6 w-6 rounded-full bg-gradient-to-r from-pink-500 to-violet-500 flex-shrink-0" />
+        <div className="w-full bg-gray-100 h-4 rounded-full dark:bg-neutral-900" />
+      </motion.div>
+    </motion.div>
+  );
+};
+
+const items = [
+  {
+    title: "Age-Related Arthritis in Animals",
+    description: (
+      <span className="text-sm">
+        Aging animals often suffer from arthritis, leading to joint pain and
+        limited mobility. This condition significantly impacts their quality of
+        life and requires early detection and management.
+      </span>
+    ),
+    header: <SkeletonOne />,
+    className: "md:col-span-1",
+    icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: "Predicting Arthritis Progression in Pets",
+    description: (
+      <span className="text-sm">
+        Early prediction of arthritis progression helps pet owners and
+        veterinarians take proactive steps in treatment and care, improving the
+        overall quality of life for aging animals.
+      </span>
+    ),
+    header: <SkeletonOne />,
+    className: "md:col-span-1",
+    icon: <IconFileBroken className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: "Data Analysis for Preventing Animal Pain",
+    description: (
+      <span className="text-sm">
+        Using data to track and analyze symptoms of arthritis in animals allows
+        for more effective intervention strategies, helping prevent pain and
+        improving animal health outcomes.
+      </span>
+    ),
+    header: <SkeletonOne />,
+    className: "md:col-span-1",
+    icon: <IconSignature className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: "Predictive Models for Senior Animal Health",
+    description: (
+      <span className="text-sm">
+        AI and predictive models can be used to analyze data on senior animals,
+        predicting the likelihood of arthritis and other age-related health
+        conditions, allowing for timely treatment and care.
+      </span>
+    ),
+    header: <SkeletonOne />,
+    className: "md:col-span-2",
+    icon: <IconTableColumn className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: "AI for Tailored Treatment Plans for Arthritis",
+    description: (
+      <span className="text-sm">
+        AI-driven systems can provide personalized treatment plans for aging
+        animals with arthritis, ensuring they receive the best possible care.
+      </span>
+    ),
+    header: <SkeletonOne />,
+    className: "md:col-span-1",
+    icon: <IconBoxAlignRightFilled className="h-4 w-4 text-neutral-500" />,
+  },
+];

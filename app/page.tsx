af6b59525ba4952636c3b032a125c0def3146a3e
Adnan <adnanali23189@gmail.com>
2024-11-23 12:50:52 +0530

    fix bug

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 import ProblemShowcaseDetail from "@/components/ProblemShowcaseDetail";
-import WayToSolve from "@/components/wayToSolve";
+import WayToSolve from "@/components/WayToSolve";
 
 export default function Home() {
   return (
